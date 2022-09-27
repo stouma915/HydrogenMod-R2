7690d98ce33b6bf1495448a7c61ec9e8a568a3de
stouma915 <sasakitouma@yahoo.ne.jp>
2022-09-27 15:04:33 +0900

    Initial commit

---
+++ src/main/scala/net/st915/hydrogenmod/HydrogenMod.scala
@@ -0,0 +1,13 @@
+package net.st915.hydrogenmod
+
+import net.minecraftforge.fml.common.Mod
+
+object HydrogenMod {
+
+  final val ModId = "hydrogenmod"
+
+}
+
+@Mod(HydrogenMod.ModId)
+final class HydrogenMod {
+}
